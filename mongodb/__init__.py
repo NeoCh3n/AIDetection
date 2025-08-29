55941756d77e4914ad2e9036ae9a16c120590179
NeoCh3n <qu2045@gmail.com>
2025-08-29 11:50:28 +0800

    mongodb: re-export submodules in __init__.py to satisfy Pylance __all__ validation

--- mongodb/__init__.py
+++ mongodb/__init__.py
@@ -1,4 +1,16 @@
-"""MongoDB utilities package."""
+"""MongoDB utilities package.
+
+This package exposes commonly used submodules at the package level
+so tools like Pylance can resolve names listed in ``__all__``.
+
+Python 3.6.8 compatible.
+"""
+
+# Re-export submodules so names exist on the package object
+from . import insert_DB as insert_DB  # noqa: F401
+from . import query_DB as query_DB  # noqa: F401
+from . import delete_DB as delete_DB  # noqa: F401
+from . import mongodb_connection as mongodb_connection  # noqa: F401
 
 __all__ = [
     "insert_DB",
@@ -6,4 +18,3 @@ __all__ = [
     "delete_DB",
     "mongodb_connection",
 ]
-
